fab770460457290caa0db2ed18be4640193f0cdf
Robin Niederdellmann <48210165+robinniederdellmann@users.noreply.github.com>
2021-10-22 08:35:50 +0200

    Update box_counts.sql

--- box_counts/box_counts.sql
+++ box_counts/box_counts.sql
@@ -2,4 +2,4 @@
 
 SELECT *
 FROM fact_tables.boxes_shipped
-WHERE country = 'US' AND hellofresh_delivery_week = '2018-W01'
+WHEEEEEEEEEEEEERE country = 'US' AND hellofresh_delivery_week = '2018-W01'
